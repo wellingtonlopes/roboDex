3d4bf60448a901e7e8e193236daf503ddbc28134
wellingtonlopes <ookami.seta@gmail.com>
2020-07-07 17:56:07 -0400

    Updates

---
+++ precache-manifest.7b0f01c0d2f34d0cf216ab55513a2db0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dc867d6855cd0117dc058c15622043",
+    "url": "/roboDex/index.html"
+  },
+  {
+    "revision": "86b0abaeedb43f08cfe0",
+    "url": "/roboDex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7591b558f810e9a9f747",
+    "url": "/roboDex/static/css/main.208e224f.chunk.css"
+  },
+  {
+    "revision": "86b0abaeedb43f08cfe0",
+    "url": "/roboDex/static/js/2.1c51aaad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboDex/static/js/2.1c51aaad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7591b558f810e9a9f747",
+    "url": "/roboDex/static/js/main.68770931.chunk.js"
+  },
+  {
+    "revision": "50acd03b263c22b75992",
+    "url": "/roboDex/static/js/runtime-main.682b3297.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboDex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
