ed6ec476e4099893cd3f215c775b091feb542d0c
wellingtonlopes <ookami.seta@gmail.com>
2020-07-07 16:11:54 -0400

    organizes project in folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,10 @@
 import React, { Fragment, Component } from 'react';
-import CardList from './CardList';
-import Scroll from './Scroll'
-import SearchBox from './SearchBox'
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll'
+import SearchBox from '../components/SearchBox'
 import './App.css';
 
 class App extends Component {
-    
     constructor() {
         super();
         this.state = {
@@ -25,10 +24,11 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter( robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter( robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
+        if (!robots.length) {
             return <h1 className='tc pv7'>Loading...</h1>
         } else {
             return (
