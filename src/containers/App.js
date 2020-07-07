121c0a7840515c9df50211575e88412413bec5be
wellingtonlopes <ookami.seta@gmail.com>
2020-07-07 18:00:08 -0400

    Adds error boundary component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll'
 import SearchBox from '../components/SearchBox'
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -37,7 +38,9 @@ class App extends Component {
                         <h1 className='f1'>RoboDex</h1>
                         <SearchBox searchChange={this.onSearchChange}/>
                         <Scroll>
-                            <CardList robots={filteredRobots}/>
+                            <ErrorBoundary>
+                                <CardList robots={filteredRobots}/>
+                            </ErrorBoundary>
                         </Scroll>
                     </div>
                 </Fragment>
